741860a8b12499a5fdf6603b466a8645d99c2122
yasir <mohammadyasirkmt@gmail.com>
2026-04-02 16:39:28 +0530

    write test cases for User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Jack Sparrow", email: "jack@example.com", role: "admin", status: "active")
+  end
+
+  test "valid user should save" do
+    assert @user.save
+  end
+
+  test "should not save without name" do
+    @user.name = nil
+    assert_not @user.save
+  end
+
+  test "should not save without email" do
+    @user.email = nil
+    assert_not @user.save
+  end
+
+  test "should not allow duplicate email" do
+    @user.save
+    duplicate_user = @user.dup
+    assert_not duplicate_user.save
+  end
 end
